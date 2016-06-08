787129c20a32a47f0642de46152738ba3f6e86e7
Claudio Walser <claudio.walser@srf.ch>
2016-06-08 09:07:15 +0200

    Simple time algorythm for alarming threshold

--- surveillance/noise-detection.py
+++ surveillance/noise-detection.py
@@ -2,27 +2,57 @@
 
 import audioop
 import pyaudio
+import time
 
 from pprint import pprint
 
 chunk = 1024
 volumeTreshold = 1024
+deviceIndex = 0
+# time in seconds of noise detection until alarming the monitoring device
+alarmingTreshold = 30
+# time in seconds for an acceptable pause which is not interrupting the alarmingTreshold
+acceptablePause = 5
+
 
 p = pyaudio.PyAudio()
 
-print(p.get_device_count())
-pprint(p.get_device_info_by_index(2))
+
 
 stream = p.open(format=pyaudio.paInt16,
                 channels=1,
                 rate=32000,
                 input=True,
-                input_device_index = 2,
+                input_device_index = deviceIndex,
                 frames_per_buffer=chunk)
 
+
+noiseStarted = False
+startTime = 0;
+
 while 1:
+  currentTime = time.time()
+
   data = stream.read(chunk)
   rms = audioop.rms(data, 2)  #width=2 for format=paInt16
   if rms >= volumeTreshold:
+    if noiseStarted == False:
+      noiseStarted = True
+      startTime = time.time()
+    else:
+      timeDiff = currentTime - startTime
+      print("time started: %s " % startTime)
+      print("current detection: %s " % time.time())
+      print("time diff since noise started: %s " % timeDiff)
+      if timeDiff >= alarmingTreshold:
+        print("Alarm Alarm")
+
     print(rms)
+
+  else:
+    if noiseStarted == True:
+      timeDiff = currentTime - startTime
+      if timeDiff > acceptablePause:
+        noiseStarted = False
+        startTime = 0
 
